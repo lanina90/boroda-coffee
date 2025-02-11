25745e2af233d5290c1d3c528edf2ff62b7446f0
katerynalanina <lanina.kateryna@gmail.com>
2025-02-11 10:18:24 +0200

    feat: admin

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -2,9 +2,12 @@ import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert('src/lib/serviceAccountKey.json'),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
   });
 }
 
 export const adminAuth = admin.auth();
-export const adminFirestore = admin.firestore();
